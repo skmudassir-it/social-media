8472f14c6af6f8be6d420f520d86b9c97b9082cf
mudassir <skmudassir.it@gmail.com>
2022-12-04 16:24:08 +0530

    backend

--- server/index.js
+++ server/index.js
@@ -8,6 +8,15 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
+import { register } from "./controllers/auth.js";
+import { createPost } from './controllers/posts.js'
+import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Posts from "./models/Post.js";
+import {users, posts } from "./data/index.js";
 
 /* Configuration */
 const __filename = fileURLToPath(import.meta.url);
@@ -16,29 +25,45 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy:"cross-origin" }));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* File Storage */
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "public/assets");
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
-const upload = multer({storage});
+const upload = multer({ storage });
+
+/* Routs with Files*/
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+/* Routes */
+app.use("/auth", authRoutes);
+app.use("/user", userRoutes);
+app.use("/post", postRoutes);
 
 /* Mongoos Setup */
 const PORT = process.env.PORT || 6001;
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
+  })
+  .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-}).catch((error) => console.log(`${error} did not connect`));
+
+    /* Add Data on Time */
+    // User.insertMany(users);
+    // Posts.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
