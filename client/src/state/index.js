335f89cb5e05648ccc50a365115e0c414f1278c3
skmudassir-it <96885904+skmudassir-it@users.noreply.github.com>
2022-12-04 13:11:15 +0000

    state

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -22,7 +22,7 @@ export const authSlice = createSlice({
       state.user = null;
       state.token = null;
     },
-    setFriend: (state, action) => {
+    setFriends: (state, action) => {
       if (state.user) {
         state.user.friends = action.payload.friends;
       } else {
@@ -34,7 +34,7 @@ export const authSlice = createSlice({
     },
     setPost: (state, action) => {
       const updatedPosts = state.posts.map((post) => {
-        if (post._id === action.payload.post_id) return action.payload.post;
+        if (post._id === action.payload.post._id) return action.payload.post;
         return post;
       });
       state.posts = updatedPosts;
@@ -42,5 +42,6 @@ export const authSlice = createSlice({
   },
 });
 
-export const {setMode, setLogin, setLogout, setFriend, setPost, setPosts}= authSlice.actions;
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
+  authSlice.actions;
 export default authSlice.reducer;
