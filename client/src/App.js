fe1ee111d74fa1f71faf4a916d7fbf14a529a6c7
mudassir <skmudassir.it@gmail.com>
2022-12-04 18:02:54 +0530

    navbar configuration

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
-import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
-import HomePage from 'sceans/homePage';
-import LoginPage from 'sceans/loginPage';
-import ProfilePage from 'sceans/profilePage';
-
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "sceans/homePage";
+import LoginPage from "sceans/loginPage";
+import ProfilePage from "sceans/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/system";
+import { themeSettings } from "theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage/>}/>
-        <Route path="/home" element={<HomePage/>}/>
-        <Route path="/profile/:userId" element={<ProfilePage/>}/>
-        
-      </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
